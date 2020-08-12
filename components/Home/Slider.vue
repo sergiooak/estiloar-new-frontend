81332cbd97ebbefeb9cc18f5ff3d0ac64a125db9
Sergio Carvalho <sergiopcn@gmail.com>
2020-08-12 10:47:38 -0300

    add slider

--- components/Home/Slider.vue
+++ components/Home/Slider.vue
@@ -1,12 +1,24 @@
 <template>
-    <div class="slider">
-        slider
-    </div>
+  <div class="slider">
+    slider
+  </div>
 </template>
 
+<script>
+import { Slider, SliderItem } from "vue-easy-slider";
+
+export default {
+  name: "HomeSlider",
+  components: {
+    Slider,
+    SliderItem
+  }
+};
+</script>
+
 <style scoped>
-.slider{
-    height: 352px;
-    @apply bg-gray-400;
+.slider {
+  height: 352px;
+  @apply bg-gray-400;
 }
 </style>
