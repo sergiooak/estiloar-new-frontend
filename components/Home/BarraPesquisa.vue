9ee91cb001ecc4976dfa055b06aad0553e4fbbb0
Sergio Carvalho <sergiopcn@gmail.com>
2020-08-12 10:51:54 -0300

    fix "search box" height

--- components/Home/BarraPesquisa.vue
+++ components/Home/BarraPesquisa.vue
@@ -2,10 +2,10 @@
   <div
     class="relative flex w-5/6 p-4 mx-auto -mt-8 bg-white border-2 rounded-lg shadow-xl border-estilo"
   >
-    <div class="w-2/5">
+    <div class="flex items-center w-2/5">
       <h2 class="text-lg font-bold uppercase text-estilo">{{ data.title }}</h2>
     </div>
-    <div class="w-3/5">
+    <div class="w-3/5 h-12">
       <Pesquisa :placeholder="data.description" />
     </div>
   </div>
